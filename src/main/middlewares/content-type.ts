27fdfc398820904001db33c303229b9f06893e25
WDelesposti <wallace.delesposti@gmail.com>
2023-03-19 01:38:36 -0300

    feat: connect to mongodb before app listen

--- src/main/middlewares/content-type.ts
+++ src/main/middlewares/content-type.ts
@@ -1,7 +1,6 @@
 import { type Request, type Response, type NextFunction } from 'express'
 
 export const contentType = (req: Request, res: Response, next: NextFunction): void => {
-  console.log('Content-Type: ')
   res.type('json')
   next()
 }
