fdf924f9d99417be61496f9360eadd53fca1683e
WDelesposti <wallace.delesposti@gmail.com>
2023-03-29 21:31:49 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signup'
 import { MissingParamError, ServerError } from '../../errors'
-import { serverError, badRequest } from '../../helpers/http-helper'
+import { serverError, badRequest } from '../../helpers/http/http-helper'
 import { type AddAccount, type HttpRequest, type Validation } from './signup-protocols'
 
 const makeAddAccount = (): AddAccount => {
