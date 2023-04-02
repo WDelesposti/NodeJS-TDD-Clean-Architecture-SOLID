d3dc21390871e3840398d82f80079f98716c793d
WDelesposti <wallace.delesposti@gmail.com>
2023-04-01 23:00:59 -0300

    refactor: move data layer interfaces to folders

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,4 +1,4 @@
-import { type AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { type AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
