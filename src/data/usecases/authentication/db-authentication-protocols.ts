d76e391027d5d071608c297d6e149df716d18831
WDelesposti <wallace.delesposti@gmail.com>
2023-04-02 23:40:59 -0300

    refactor: move DbAuthentication protocols to a single file

---
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -0,0 +1,7 @@
+
+export * from '../../../domain/usecases/authentication'
+export * from '../add-account/db-add-account-protocols'
+export * from '../../protocols/db/load-account-by-email-repository'
+export * from '../../protocols/criptography/hash-comparer'
+export * from '../../protocols/criptography/token-generator'
+export * from '../../protocols/criptography/update-access-token-repository'
