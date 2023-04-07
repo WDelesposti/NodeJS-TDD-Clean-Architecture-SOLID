b73ad46e237b585231e1969642f1374b070b2144
WDelesposti <wallace.delesposti@gmail.com>
2023-04-07 09:20:28 -0300

    test: ensure AccountMongoRepository returns null if loadByEmail returns null

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -49,4 +49,10 @@ describe('Account Mongo Repository', () => {
 
     expect(account).toBeTruthy()
   })
+
+  test('Should return null if loadByEmail fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('any_email@mail.com')
+    expect(account).toBeFalsy()
+  })
 })
