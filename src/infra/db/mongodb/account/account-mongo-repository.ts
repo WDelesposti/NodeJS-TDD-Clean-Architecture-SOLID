426d15f6b3aa9b4e353c38137f1514e91272edda
WDelesposti <wallace.delesposti@gmail.com>
2023-04-08 23:47:36 -0300

    refactor: improve file names

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,5 +1,5 @@
-import { type AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
-import { type LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
+import { type AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
+import { type LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
 import { type AccountModel } from '../../../../domain/models/account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
--- src/main/factories/signup/signup-validation-factory.spec.ts
+++ src/main/factories/signup/signup-validation-factory.spec.ts
@@ -1,7 +1,7 @@
 import { EmailValidation, RequiredFieldValidation, ValidationComposite, CompareFieldsValidation } from '../../../presentation/helpers/validators'
 import { type Validation } from '../../../presentation/protocols/validation'
 import { type EmailValidator } from '../../../presentation/protocols/email-validator'
-import { makeSignUpValidation } from './signup-validation'
+import { makeSignUpValidation } from './signup-validation-factory'
 
 jest.mock('../../../presentation/helpers/validators/validation-composite')
 
